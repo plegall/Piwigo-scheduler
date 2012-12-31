c0d1c8e48dc5e8a11072dbf730a38867ba490529
riopwg <riopwg@piwigo.org>
2012-12-31 10:52:55 +0000

    Language IT
    git-svn-id: http://piwigo.org/svn/extensions/scheduler@19691 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,7 @@
+<?php
+$lang['Scheduler'] = 'Pianificatore';
+$lang['Schedule photo'] = 'Pianificare la foto';
+$lang['Schedule for'] = 'Pianificare per';
+$lang['Now'] = 'Ora';
+$lang['Minute'] = 'Minuto';
+?>
